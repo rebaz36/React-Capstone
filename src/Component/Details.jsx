6e5dbf4be02ee0c065f5c22ee0cc1a1219add2d1
rebaz36 <rebaz415@gmail.com>
2022-11-22 23:56:33 +0300

    display crypto details

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -1,7 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { getDetail } from '../Redux/detailSlice';
 
 const Detail = () => {
@@ -13,7 +16,47 @@ const Detail = () => {
   });
   return (
     <Container>
-      hello
+      <Row>
+        <Col className="welcome">
+          <div className="welcome-body">
+            <Image src={detail.icon} />
+            <p className="detail-price">
+              $
+              {detail.price}
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} className="detail-col-odd">
+          <span className="header">Coin name :</span>
+          <span>{detail.name}</span>
+        </Col>
+        <Col xs={12} className="detail-col">
+          <span className="header">Coin symbol :</span>
+          <span>{detail.symbol}</span>
+        </Col>
+        <Col xs={12} className="detail-col-odd">
+          <span className="header">Ranking :</span>
+          <span>{detail.rank}</span>
+        </Col>
+        <Col xs={12} className="detail-col">
+          <span className="header">Price :</span>
+          <span>{detail.price}</span>
+        </Col>
+        <Col xs={12} className="detail-col-odd">
+          <span className="header"> Market cap :</span>
+          <span>{detail.marketCap}</span>
+        </Col>
+        <Col xs={12} className="detail-col">
+          <span className="header"> Price change week :</span>
+          <span>{detail.priceChange1w}</span>
+        </Col>
+        <Col xs={12} className="detail-col-odd">
+          <span className="header"> Avail. supply :</span>
+          <span>{detail.availableSupply}</span>
+        </Col>
+      </Row>
     </Container>
   );
 };
