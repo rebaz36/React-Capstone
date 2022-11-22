526a6a5e29b62287d8b68169736c99e381f7c548
rebaz36 <rebaz415@gmail.com>
2022-11-22 23:53:42 +0300

    show data on home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+/* eslint-disable max-len */
+import { useDispatch, useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from 'react';
+import { getAssests } from '../Redux/cryptoSlice';
 
-export default function Home() {
+const Home = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div>Home</div>
+    <Container>
+      <Row>
+        <Col className="">
+          <div className="">
+            <p>Welcome!</p>
+            <p>CryptoHouse</p>
+          </div>
+        </Col>
+      </Row>
+      <Row className="">
+        <div className="">
+          <span>Filter:</span>
+          <input type="text" size="sm" value={searchcoin} onChange={onSearch} />
+        </div>
+      </Row>
+      <Row className="">
+        {searched.map((asset) => (
+          <Col
+            xs={6}
+            className=""
+            Key={`${asset.asset_id}${Math.random * 10}`}
+          >
+            <Link
+              className=""
+              Key={asset.asset_id}
+              to={`/crypto/${asset.id}`}
+            >
+              <Row className="">
+                <p>{asset.price}</p>
+              </Row>
+              <Row>
+                <Col>
+                  <Image className="" src={asset.icon} />
+                </Col>
+                <Col className="">
+                  <span className="">{asset.symbol}</span>
+                  <span className="">{asset.name}</span>
+                </Col>
+              </Row>
+              <Row />
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
-}
+};
+export default Home;
