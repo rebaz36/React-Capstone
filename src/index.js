5b42154136cdf27b37ab098eecae3a695afc8466
rebaz36 <rebaz415@gmail.com>
2022-11-22 13:45:32 +0300

    add react router and Redux Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react';
 import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './Redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
