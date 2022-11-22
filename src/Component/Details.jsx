2e43b4b606b752eaef77dc827d30c4701c427dca
rebaz36 <rebaz415@gmail.com>
2022-11-22 23:55:39 +0300

    get data from the store

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getDetail } from '../Redux/detailSlice';
 
-export default function Details() {
+const Detail = () => {
+  const data = useParams();
+  const dispatch = useDispatch();
+  const detail = useSelector((state) => state.detail);
+  useEffect(() => {
+    dispatch(getDetail(data.id));
+  });
   return (
-    <div>Details</div>
+    <Container>
+      hello
+    </Container>
   );
-}
+};
+
+export default Detail;
