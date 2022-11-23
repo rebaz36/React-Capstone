c5de9bf876fea1498425282ddc5af891b7a8fdf1
rebaz36 <rebaz415@gmail.com>
2022-11-24 01:15:57 +0300

    add navbar styles

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -3,6 +3,7 @@ import { Image } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getDetail } from '../Redux/detailSlice';
+import Nav from './Nav';
 
 const Detail = () => {
   const data = useParams();
@@ -16,70 +17,73 @@ const Detail = () => {
     currency: 'USD',
   });
   return (
-    <div className="Container">
-      <div className="Icon_Container">
-        <Image src={detail.icon} />
-        <p>
-          ~
-          {formatter.format(
-            Math.round(detail.price) === 0
-              ? detail.price
-              : Math.round(detail.price),
-          )}
-        </p>
-      </div>
-      <div className="Details">
-        <div className="background">
-          <span>Coin name :</span>
-          <span>{detail.name}</span>
-        </div>
-        <div className="background">
-          <span>Coin symbol :</span>
-          <span>{detail.symbol}</span>
-        </div>
-        <div className="background">
-          <span>Ranking :</span>
-          <span>{detail.rank}</span>
-        </div>
-        <div className="background">
-          <span>Price :</span>
-          <span>
-            {' '}
+    <>
+      <Nav back />
+      <div className="Container">
+        <div className="Icon_Container">
+          <Image src={detail.icon} />
+          <p>
+            ~
             {formatter.format(
               Math.round(detail.price) === 0
                 ? detail.price
                 : Math.round(detail.price),
             )}
-          </span>
+          </p>
         </div>
-        <div className="background">
-          <span> Cap :</span>
-          <span>
-            {' '}
-            {formatter.format(
-              Math.round(detail.marketCap) === 0
-                ? detail.price
-                : Math.round(detail.marketCap),
-            )}
-          </span>
-        </div>
-        <div className="background">
-          <span> Price change week :</span>
-          <span>{detail.priceChange1w}</span>
-        </div>
-        <div className="background">
-          <span> Supply :</span>
-          <span>
-            ~
-            {formatter.format(
-              Math.round(detail.availableSupply) === 0
-                ? detail.price
-                : Math.round(detail.availableSupply),
-            )}
-          </span>
+        <div className="Details">
+          <div className="background">
+            <span>Coin name :</span>
+            <span>{detail.name}</span>
+          </div>
+          <div className="background">
+            <span>Coin symbol :</span>
+            <span>{detail.symbol}</span>
+          </div>
+          <div className="background">
+            <span>Ranking :</span>
+            <span>{detail.rank}</span>
+          </div>
+          <div className="background">
+            <span>Price :</span>
+            <span>
+              {' '}
+              {formatter.format(
+                Math.round(detail.price) === 0
+                  ? detail.price
+                  : Math.round(detail.price),
+              )}
+            </span>
+          </div>
+          <div className="background">
+            <span> Cap :</span>
+            <span>
+              {' '}
+              {formatter.format(
+                Math.round(detail.marketCap) === 0
+                  ? detail.price
+                  : Math.round(detail.marketCap),
+              )}
+            </span>
+          </div>
+          <div className="background">
+            <span> Price change week :</span>
+            <span>{detail.priceChange1w}</span>
+          </div>
+          <div className="background">
+            <span> Supply :</span>
+            <span>
+              ~
+              {formatter.format(
+                Math.round(detail.availableSupply) === 0
+                  ? detail.price
+                  : Math.round(detail.availableSupply),
+              )}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
