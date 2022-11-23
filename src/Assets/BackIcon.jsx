160382f84c0b48dfa9caec3f6049cc87fe5ead67
rebaz36 <rebaz415@gmail.com>
2022-11-24 01:14:36 +0300

    add back icon

---
+++ src/Assets/BackIcon.jsx
@@ -0,0 +1,10 @@
+const BackIcon = () => (
+  <svg width={16} height={16} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16 7H3.83l5.59-5.59L8 0 0 8l8 8 1.41-1.41L3.83 9H16V7Z"
+      fill="#2b6ec6"
+    />
+  </svg>
+);
+
+export default BackIcon;
