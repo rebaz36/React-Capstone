7095fa08b3a74d62644cd30533c179be920f6de4
rebaz36 <rebaz415@gmail.com>
2022-11-24 09:54:28 +0300

    fix import

--- src/__Test__/Home.test.js
+++ src/__Test__/Home.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import store from '../redux/configureStore';
+import store from '../Redux/configureStore';
 import Home from '../Component/Home';
 
 describe('Home component', () => {
